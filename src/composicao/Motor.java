d96388828d6609bb2b31c6ca65449854b5f5f126
William Xavier <willian.xavier@alunos.sc.senac.br>
2022-09-19 19:35:16 -0300

    Composição

---
+++ src/composicao/Motor.java
@@ -0,0 +1,16 @@
+package composicao;
+
+public class Motor {
+    public int potencia;
+
+    public int getPotencia() {
+        return potencia;
+    }
+
+    public void setPotencia(int potencia) {
+        this.potencia = potencia;
+    }
+    
+    
+    
+}
