ea21428d86a06533d7911a93ccf9bd4f4e807e6b
William Xavier <willian.xavier@alunos.sc.senac.br>
2022-09-19 19:59:46 -0300

    tres formas composicao

--- src/composicao/Composicao.java
+++ src/composicao/Composicao.java
@@ -2,9 +2,18 @@ package composicao;
 
 public class Composicao {
     public static void main(String[] args) {
+        // Primeira forma
         //Carro carro = new Carro();
         //Carro carro2 = new Carro(2000);
-        Carro carro3 = new Carro();
-        carro3.ligarCarro(2000);
+        // Segunda forma
+        //Carro carro3 = new Carro();
+        //carro3.ligarCarro(2000);
+        //Terceira forma
+        Carro carro4 = new Carro();
+        Motor motor = new Motor();
+        Motor motor2 = new Motor(2000);
+        
+        carro4.setMotor(motor2);
+        
     }
 }
